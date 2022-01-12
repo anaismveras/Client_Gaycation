d9271439f1e334efe9c0c93274d2c9a8838caefc
Emily Barwinczak <ebarw@MacBook-Air.local>
2022-01-11 22:19:09 -0500

    buttons routing to search page and more formatting and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,14 @@
 import './Home.css'
 import gaycation from '../images/gaycation.png'
+import { Link } from 'react-router-dom'
+
+const buttonStyle = {
+	backgroundColor: 'white',
+	color: '#424874',
+	borderRadius: '4px',
+	// border: '#424874',
+	width: '150px'
+}
 
 const Home = (props) => {
 	// const { msgAlert, user } = props
@@ -7,8 +16,20 @@ const Home = (props) => {
 
 	return (
 		<div className="homePage">
-			<h1>Find safe and exciting <br></br>LGBTQIA+ friendly vacation destinations</h1>
-			<p><img className="homeImg" src={gaycation} alt="homeImage"></img></p>
+			<div className="firstRow">
+				<h1>Find safe and exciting <br></br>LGBTQIA+ friendly vacation destinations</h1><br></br>
+				<div id="homePageBtns">
+					<Link to="/sign-up">
+						<button style={buttonStyle}><small>Sign Up</small></button>
+					</Link> <h6></h6>
+					<Link to="/sign-in">
+						<button style={buttonStyle}><small>Sign In</small></button>
+					</Link>
+				</div>
+			</div>
+			<div className="secondRow">
+				<p><img className="homeImg" src={gaycation} alt="homeImage"></img></p>
+			</div>
 		</div>
 	)
 }
