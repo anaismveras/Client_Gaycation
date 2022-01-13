725d8563f02c6f14d05bbceed0a4b0c588610743
anaismveras <verasmanais@gmail.com>
2022-01-12 20:20:03 -0500

    removed commented out code

--- src/components/main/UsersGaycations.js
+++ src/components/main/UsersGaycations.js
@@ -1,40 +1,40 @@
-// import React from "react"
-// import axios from "axios"
+import React from "react"
+import axios from "axios"
 
-// const UsersGaycations = () => {
-//     // for sanity check
-//     const [likedDestinations, setLikedDestinations] = [{
-//         city: '',
-//         state: '',
-//         country: '',
-//         lgbtRating: '',
-//         image_url: '',
-//         description: '',
-//         owner: '',
-//         business: [],
-//         reviews: [],
-//         roadGoatId: ''
-//     }]
+const UsersGaycations = () => {
+    // for sanity check
+    const [likedDestinations, setLikedDestinations] = [{
+        city: '',
+        state: '',
+        country: '',
+        lgbtRating: '',
+        image_url: '',
+        description: '',
+        owner: '',
+        business: [],
+        reviews: [],
+        roadGoatId: ''
+    }]
 
-//     const postGaycation = () => {
-//         axios.post(`http://localhost:8000/destinations`, {
-//             city: '',
-//             state: '',
-//             country: '',
-//             lgbtRating: '',
-//             image_url: '',
-//             description: '',
-//             owner: '',
-//             roadGoatId: ''
-//         }) 
-//     }
+    const postGaycation = () => {
+        axios.post(`http://localhost:8000/destinations`, {
+            city: '',
+            state: '',
+            country: '',
+            lgbtRating: '',
+            image_url: '',
+            description: '',
+            owner: '',
+            roadGoatId: ''
+        }) 
+    }
 
-//     return (
-//         <h1>Your Gaycation Destinations:</h1>
-//     )
+    return (
+        <h1>Your Gaycation Destinations:</h1>
+    )
 
 
 
-// }
+}
 
-// export default UsersGaycations
+export default UsersGaycations
