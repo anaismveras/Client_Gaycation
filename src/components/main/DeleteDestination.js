43c7e0bdf620f095bbcecd05b74b0cebc90ff0bc
Emily Barwinczak <ebarw@MacBook-Air.local>
2022-01-13 10:35:27 -0500

    debugging

--- src/components/main/DeleteDestination.js
+++ src/components/main/DeleteDestination.js
@@ -1,6 +1,6 @@
-// import React from 'react'
-// import axios from 'axios'
-// import { useState } from 'react'
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 
 
-// export default DeleteDestination
+export default DeleteDestination
