d161d4d24d83b87d98710df3c6624734a5172a16
anaismveras <verasmanais@gmail.com>
2022-01-12 20:58:46 -0500

    added props to function

--- src/components/main/UsersGaycations.js
+++ src/components/main/UsersGaycations.js
@@ -4,21 +4,21 @@ import axios from "axios"
 
 
 
-const UsersGaycations = () => {
+const UsersGaycations = (props) => {
     
     const [gaycations, setGaycations] = useState([])
 
-    // const getGaycations = (e) => {
-    //     e.preventDefault()
-    //     axios.get('http://localhost:8000/destinations',{
-    //         headers: {
-    //             "Authorization": `Bearer ${props.user.token}`
-    //         }
-    //     })
-    //     .then(gaycations => {
-    //         console.log('this is faves', gaycations)
-    //     })
-    // }
+    const getGaycations = (e) => {
+        e.preventDefault()
+        axios.get('http://localhost:8000/destinations',{
+            headers: {
+                "Authorization": `Bearer ${props.user.token}`
+            }
+        })
+        .then(gaycations => {
+            console.log('this is faves', gaycations)
+        })
+    }
 
     return (
         <h1>Your Gaycation Destinations:</h1>
