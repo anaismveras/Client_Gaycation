d9bf76f418b1808b32d5fb37dd79314757b414ff
Emily Barwinczak <ebarw@MacBook-Air.local>
2022-01-13 10:31:49 -0500

    debugging

---
+++ src/components/main/DeleteDestination.js
@@ -0,0 +1,6 @@
+// import React from 'react'
+// import axios from 'axios'
+// import { useState } from 'react'
+
+
+// export default DeleteDestination
--- src/components/main/UsersGaycations.js
+++ src/components/main/UsersGaycations.js
@@ -4,29 +4,7 @@ import { useEffect, Link, useState } from "react"
 
 const UsersGaycations = (props) => {
 
-    // const [gaycations, setGaycations] = useState([])
 
-    // // create function that called the delete route
-    // // const handleDelete = () => {
-
-    // // }
-    // const getGaycations = () => {
-	// 	if (user !== null) {
-	// 		axios.get('http://localhost:8000/destinations',{
-	// 			headers: {
-	// 				"Authorization": `Bearer ${user.token}`
-	// 			}
-	// 		})
-	// 		.then(foundGaycations => {
-	// 			console.log('this is faves', foundGaycations)
-	// 			setGaycations(foundGaycations)
-	// 		})
-	// 	}
-    // }
-
-	// useEffect(() => {
-	// 	getGaycations()
-	// }, [props.user])
 
     const mapGaycations = props.gaycations.map(place => {
         // console.log('this is place length', place.image_url)
