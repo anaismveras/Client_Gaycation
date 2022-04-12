1cd14941208217735f3374c0b704ac38dbdfa8b0
anaismveras <verasmanais@gmail.com>
2022-04-12 11:35:54 -0400

    removed slash from production

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://gaycation.herokuapp.com/',
+	production: 'https://gaycation.herokuapp.com',
 	development: 'http://localhost:8000',
 }
 
